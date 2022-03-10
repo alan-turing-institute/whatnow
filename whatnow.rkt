9f6d5451e5236e03860ee08ee91573a3bb7e87ec
James Geddes <jgeddes@turing.ac.uk>
2022-03-10 15:51:21 +0000

    Minor edits

--- whatnow.rkt
+++ whatnow.rkt
@@ -20,18 +20,20 @@ TODO:
 (module+ main
   (require gregor
          "schedule.rkt"
-         "table.rkt")
-  (define the-schedule (get-the-schedule))
+         mondrian)
+  (define *the-schedule* (get-the-schedule))
 
   (displayln "Successfully obtained")
-  (printf " - ~a people;\n" (length (schedule-people the-schedule)))
-  (printf " - ~a projects;\n" (length (schedule-projects the-schedule)))
-  (printf " - ~a programmes; and\n" (length (schedule-programmes the-schedule)))
-  (printf " - ~a assignments\n" (length (schedule-assignments the-schedule)))
+  (printf " - ~a people;\n" (length (schedule-people *the-schedule*)))
+  (printf " - ~a projects;\n" (length (schedule-projects *the-schedule*)))
+  (printf " - ~a programmes; and\n" (length (schedule-programmes *the-schedule*)))
+  (printf " - ~a assignments\n" (length (schedule-assignments *the-schedule*)))
 
-  (for ([row (people-table the-schedule (today) (+days (today) 180))])
-    (displayln row))
-  
+  (displayln
+   (table-pretty-print
+    (people-table *the-schedule* (today) (+days (today) 180))
+    #:rule-maker (make-standard-rule-maker "=-" "  ")))
+
   )
 
 
@@ -40,22 +42,18 @@ TODO:
 
 People view
 
-(summarise-people-by-week sched date-from date-to)
-- sched : schedule?
-- date-from : a date?, which is 'rounded down' to the most recent Monday
-- date-to : a date?, which is 'rounded up' to the next Sunday.
-
 Produces a week-by-week summary of the staffing level of each person
 
 |#
 
 (require racket/vector
+         racket/list
          gregor
          gregor/period)
 
 (require "schedule.rkt"
          "db/types.rkt"
-         "table.rkt")
+         mondrian)
 
 
 ;; people-table : schedule? date? date? -> Table?
@@ -63,18 +61,7 @@ Produces a week-by-week summary of the staffing level of each person
 ;; Number of seconds in five working days
 ;; (Note that, however, Placeholders assume 7 day weeks)
 (define MAX-WEEKLY-EFFORT (* 5 8 60 60))
-
-(define MINOR-VERTICAL-RULE   #\:)
-(define MAJOR-VERTICAL-RULE   #\|)
-(define MINOR-HORIZONTAL-RULE #\-)
-(define MAJOR-HORIZONTAL-RULE #\=)
-(define MINOR-VERTICAL-MINOR-HORIZONTAL #\÷)
-(define MINOR-VERTICAL-MAJOR-HORIZONTAL #\:)
-(define MAJOR-VERTICAL-MINOR-HORIZONTAL #\+)
-(define MAJOR-VERTICAL-MAJOR-HORIZONTAL #\=)
-
-;; Naming convention:
-;; `person:foo` is a dictionary with person-id as the key and foo as the data
+(define MONTH-NAMES #("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"))
 
 (define (people-table sched date-from date-to)
   (define (person-full-name p)
@@ -84,69 +71,71 @@ Produces a week-by-week summary of the staffing level of each person
   (define weekv         
     (period->weeks date-from date-to)) 
 
+  (define nweeks
+    (vector-length weekv))
+
+  ;; Group weeks by "ISO month"
+  (define month-groups
+    (group-by values (map iso-month-of-date (vector->list weekv)) =))
+
+  (define nweeks-per-month
+    (map length month-groups))
+
+  ;; group-by-b : [list-of integer?] [list-of any/c] -> [list-of [list-of any/c]]
+  (define (group-by-n ns ws)
+    (for/fold ([groups  '()]
+               [ws      ws]
+               #:result (reverse groups))
+              ([n ns])
+      (let-values ([(this rest) (split-at ws n)])
+        (values (cons this groups) rest))))
+
   ;; [hash-of person-id? [vector-of char?]]
   (define person:weekly-effort                  
     (for/hash ([(id efforts) (in-hash (tabulate-people-by-week sched weekv))])
       (values id (vector-map weekly-effort->char efforts))))
-
-  ;; [assoc? person-id? string?]
-  (define person:full-name
-    (map
-     (λ (p) (cons (person-id p) (person-full-name p)))
-     (schedule-people sched)))
-
-  (define max-row-header-length
-    (apply max (map (λ (p) (string-length (cdr p))) person:full-name)))
-
-  (define max-cols
-    (apply max (map vector-length (hash-values person:weekly-effort))))
   
-  (define rows
-    (map (λ (p) (hash-ref person:weekly-effort (car p) (make-vector max-cols #\space)))
-         person:full-name))
-
-  (define (pad-right-or-truncate txt width)
-    (let ([txt-width (string-length txt)])
-      (cond
-        [(< width txt-width) (substring txt 0 width)]
-        [(> width txt-width) (string-append txt (make-string (- width txt-width) #\space))]
-        [else                txt])))
-
-  ;; hd : row header
-  ;; cs : list of column content
-  ;; hd-width : width of row header (will be truncated or padded)
-  (define (make-row/text hd cs hd-width)
-    (string-append
-     (pad-right-or-truncate hd hd-width)
-     " " (string MAJOR-VERTICAL-RULE) " "
-     ((compose list->string vector->list) cs)))
+  (define (person-weekly-effort-chars p)
+    (map (λ (c) (table-cell (string c)))
+         (vector->list
+          (hash-ref person:weekly-effort
+                    (person-id p)
+                    (make-vector nweeks #\∅)))))
   
-  (map (λ (hd cs) (make-row/text hd cs max-row-header-length))
-       (map cdr person:full-name) rows)
-
-)
-
-
+  (table-col
+   (table-row "Person" "Weekly allocation")
+   (table-rowwise-bind
+    (list
+     (table-row "" (table-colwise-bind
+                    (map (λ (mg) (table-cell (vector-ref MONTH-NAMES (- (car mg) 1)) #:align 'right))
+                         month-groups)))
+     (table-rowwise-bind                                           ; Body of table
+      (map
+       (λ (p)
+         (table-row (table-cell (person-full-name p) #:pad? #t)
+                    (table-colwise-bind                                  
+                     (map table-colwise-bind 
+                          ; Group into months
+                          (group-by-n nweeks-per-month (person-weekly-effort-chars p))))))
+       (schedule-people sched))
+      )))))
 
 ;; numeric? -> char?
 (define (weekly-effort->char weekly-effort)
   (let ([rate (/ weekly-effort MAX-WEEKLY-EFFORT)])
     (cond
-      [(<  rate 0.05) #\space]
-      [(<= rate 0.20) #\.]
-      [(<= rate 0.50) #\o]
-      [(<= rate 0.80) #\x]
-      [(<= rate 0.95) #\O]
-      [(=  rate 1.0)  #\X]
-      [else           #\#])))
+      [(<= rate 0.05) #\space]
+      [(<= rate 0.20) #\·]
+      [(<= rate 0.50) #\›]
+      [(<= rate 0.95) #\*]
+      [(<= rate 1.0)  #\X]
+      [else           #\!])))
 
 
 ;; Return a map of person-id to a vector of weekly staffing levels (in seconds) for each person-id.
 ;;
 ;; schedule? [Vector-of? date?] -> [Hash? person-id? [vector-of number?]]
 (define (tabulate-people-by-week sched weekv)
-
-  ;; For each person, add up the weekly staffing levels of all assignments, week by week
   (foldl
    (λ (assgnmnt rates)
      (let ([weekly-rates (weekify-allocation weekv assgnmnt)])
@@ -159,7 +148,7 @@ Produces a week-by-week summary of the staffing level of each person
 
 
 ;; Decompose an allocation into weekly totals. The total for a week is the rate multiplied by the
-;; number of non-weekend days in the week overlap the assignment.
+;; number of non-weekend days in the week which overlap the assignment.
 ;;
 ;; [Vector-of date?] allocation? -> [Vector-of numeric?]
 (define (weekify-allocation weekv a)
@@ -196,6 +185,18 @@ Produces a week-by-week summary of the staffing level of each person
                #:break (date>=? (+weeks start wk) date-to))
       (+weeks start wk))))
 
+;; Return the "ISO month" of the date. The "ISO month" (which is not an official term of ISO 8601)
+;; here means "the month in which the Thursday of the week falls."
+;; Weeks start on Monday and end on Sunday (as ISO 8601)
+;; date? -> [integer-in 1 12]
+(define (iso-month-of-date dt)
+  (define thursday (+days dt (- 4 (->iso-wday dt))))
+  (->month thursday))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (iso-month-of-date (date 2021 10 1)) 9)
+  )
 
 ;; Return most recent Monday not later than `dt`
 ;;
