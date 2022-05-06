0fb6cbe632232e58e6f153cbdd1ca7e16a11db1f
Oliver Strickson <ostrickson@turing.ac.uk>
2022-05-06 11:56:24 +0100

    Fix Mondrian git URL for info

--- info.rkt
+++ info.rkt
@@ -4,4 +4,4 @@
 (define version "0.0.1")
 (define scribblings '(("scribblings/whatnow.scrbl")))
 
-(define deps '("basedir" "gregor" "https://github.com/alan-turing-institute/mondrian"))
+(define deps '("basedir" "gregor" "https://github.com/alan-turing-institute/mondrian.git"))
